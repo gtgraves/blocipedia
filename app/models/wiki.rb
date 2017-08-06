90da853667c1f55d013a288f476c29f392b37013
George Graves <gtgraves@gmail.com>
2017-08-06 12:03:04 -0500

    User Story 5 - Seeding Data: Completed

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,9 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, length: { minimum: 5 }, presence: true
+  default_scope { order('title ASC') }
+
+  validates :title, length: { minimum: 3 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :user, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Create users
+5.times do
+  user = User.create!(
+    name: Faker::Internet.unique.user_name,
+    email: Faker::Internet.unique.safe_email,
+    password: Faker::Internet.password(8)
+  )
+
+  user.confirm
+end
+users = User.all
+
+#Create Wikis
+50.times do
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::RockBand.unique.name,
+    body: Faker::MostInterestingManInTheWorld.quote
+  )
+end
+
+# Create an admin user
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+admin.confirm
+
+# Create a standard user
+standard = User.create!(
+  name: 'Standard User',
+  email: 'member@example.com',
+  password: 'helloworld',
+)
+standard.confirm
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
