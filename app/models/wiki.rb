ca5d76379270c10644fd36553c0e6259362644a1
George Graves <gtgraves@gmail.com>
2017-08-13 12:58:42 -0500

    User Story 7 - Private Wikis: Completed

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,7 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('title ASC') }
+  scope :visible_to, -> (user) { (user.premium? || user.admin?) ? all : where(:private => false) }
 
   validates :title, length: { minimum: 3 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
